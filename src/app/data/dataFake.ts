11d01a9c3927db58e2668e00adbd63d1665f30a5
JoseMarcosNF <jose.marcos.faria@hotmail.com>
2023-10-04 10:26:21 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,56 @@
+
+export const dataFake = [{
+    "id": "1",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/"+
+    "%20%20%20%20images?q=tbn:ANd9GcQI4k-7QNbzdO-WRUqeeK6dCIvwQrWv_Xrr32At11AtUgklfZWAyrWAIfaMyDPWhQTsQZo&usqp=CAU",
+    "title": "Saiu a nova versão do Angular.",
+    "description": "A versão mais recente do Angular, no momento desta tradução,"+
+     "é a versão 16.1.2, de 21 de junho de 2023, compatível com as versões 16.14.0 a 18.10.0 do NodeJS, 4.9.3 a 5.2.0 do TypeScript e 6.5.3 a 7.4.0 do RxJS."
+},
+{
+    "id": "2",
+    "photoCover": "https://media.seudinheiro.com/cdn-cgi/image/"+
+    "%20%20%20%20%20%20%20%20fit=contain,width=715&,format=auto/uploads/2023/02/o-senhor-dos-aneis-715x402.jpg",
+    "title": "Warner Bros fecha acordo para lançar novos filmes de Senhor dos Anéis",
+    "description": "O mapa da Terra Média é um antigo conhecido dos fãs de Senhor dos Anéis — e está prestes a ter novos caminhos explorados na frente das telonas. Nove anos depois do último lançamento da trilogia de O Hobbit, a Warner Bros anunciou que pretende lançar novos filmes baseados nos livros de J.R.R Tolkien."+
+    "A Warner Bros Discovery e a New Line Cinema fecharam um acordo com a Embracer — que detém os direitos das histórias de Tolkien desde a compra da Middle-Earth Enterprises, em 2022 — para produzir  vários  longas-metragens baseados nas obras."+
+    "O comunicado foi feito na noite de ontem pelo CEO da empresa, David Zaslav, durante a teleconferência de resultados trimestrais da companhia, mas não revelou informações sobre valores do negócio."+
+     "Apesar de todo o escopo e detalhes cuidadosamente incluídos nas duas trilogias, o vasto, complexo e deslumbrante universo sonhado por JRR Tolkien permanece amplamente inexplorado no cinema , escreveram os co-CEOs da Warner Bros Pictures Group, Michael De Luca e Pam Abdy, em nota à imprensa."+
+     "A oportunidade de convidar os fãs a se aprofundarem no mundo cinematográfico da Terra-média é uma honra, e estamos entusiasmados em fazer parceria com a Middle-Earth Enterprises e a Embracer nesta aventura."+
+    "Vale destacar que, apesar de a Warner e a New Line Cinema terem produzido as duas trilogias nos cinemas, hoje, os direitos dos filmes, jogos, mercadorias, parques temáticos e produções ao vivo de  O Senhor dos Anéis  pertencem à Embracer."+
+    "Já os direitos televisivos da história estão sob o guarda-chuva da Amazon. Após a aquisição dos direitos, a empresa de streaming lançou a série  O Senhor dos Anéis: Os Anéis do Poder  no ano passado, que tornou-se uma das maiores estreias do Prime Video."+
+    "O que esperar dos novos filmes?"+
+    "Os chefes da divisão de filmes da Warner Bros, Mike De Luca e Pam Abdy, destacaram que os novos filmes sobre o universo tolkeniano não necessariamente vão revisitar assuntos e locais já explorados pelas produções anteriores."+
+    "Segundo informações da Variety, nenhum cineasta foi contratado para os projetos ainda, apesar de Peter Jackson, diretor das duas trilogias, ter revelado à revista que ele e seus principais colaboradores foram atualizados de todas as novidades pela Warner e a Embracer."+
+    "“Estamos ansiosos para falar mais com eles para ouvir sua visão para o futuro da franquia”, disse Jackson."+
+    "Atualmente, a New Line e a Warner Bros. Animation já estão produzindo um spin-off em estilo anime, “O Senhor dos Anéis: A Guerra dos Rohirrim”, programado para chegar aos cinemas em 12 de abril de 2024."+
+    "A história da produção deve se passar 183 anos antes dos eventos do primeiro filme da trilogia original e contar o destino da Casa de Helm Hammerhand, o Rei de Rohan."+
+    "*Com informações de Variety e Box Office Mojo."
+
+},
+{
+    "id": "3",
+    "photoCover": "https://windowsclub.com.br/wp-content/uploads/2023/08/baldurs-gate.jpg",
+    "title": "Baldur's Gate 3 se torna o jogo mais bem avaliado de todos os tempos para PC",
+    "description": "Lançado recentemente, Baldur’s Gate 3, desenvolvido pela Larian Studios,"+
+    "conquistou o título de jogo de PC mais bem avaliado de todos os tempos, superando até mesmo"+
+     "gigantes como The Legend of Zelda: Tears of the Kingdom. Com críticas positivas e um sucesso"+
+      "estrondoso de vendas, este RPG baseado em Dungeons & Dragons já é considerado um sucesso absoluto."+
+      "Lançado após um extenso período de acesso antecipado, Baldur’s Gate 3 não apenas quebrou recordes"+
+       "de jogadores simultâneos no Steam, mas também se tornou o jogo mais vendido na plataforma da Valve."+
+        "A expectativa é que o jogo também faça sua estreia no PlayStation 5 no próximo mês, com uma versão"+
+         "para Xbox Series atualmente em desenvolvimento."+
+         "O que torna este feito ainda mais impressionante é a concorrência acirrada que enfrentou. Até recentemente, Tears of the Kingdom parecia ser o favorito incontestável do ano. No entanto, com uma pontuação de 97 no Metacritic, Baldur’s Gate 3 superou o título da Nintendo por um ponto, solidificando sua posição no topo."+
+    "Vale ressaltar que, apesar de sua alta pontuação, Baldur’s Gate 3 foi avaliado por um número menor de críticos em comparação a outros títulos. Por exemplo, enquanto Tears of the Kingdom foi avaliado por 145 críticos, o jogo da Larian Studios foi avaliado por apenas 21. Isso sugere que a pontuação pode variar à medida que mais análises são publicadas."+
+    " Fonte: https://www.adrenaline.com.br/games/pc-games/avaliacao-baldurs-gate-3/"
+},
+{
+    "id": "4",
+    "photoCover": "https://www.theenemy.com.br/webstories/xbox-compra-activision-aprovada/assets/2.jpg",
+    "title": "Xbox: Compra da Activision é aprovada. É OFICIAL!",
+    "description": "É OFICIAL! Os acionistas da Activision Blizzard aprovaram nesta quinta-feira (28)"+
+    "a proposta da transação da compra da empresa pela Microsoft. A aprovação aconteceu na Reunião"+
+    "Especial de Acionistas realizada nesta data com mais de 98% dos votos a favor."+
+    " Fonte: https://www.theenemy.com.br/webstories/xbox-compra-activision-aprovada"
+}
+];
